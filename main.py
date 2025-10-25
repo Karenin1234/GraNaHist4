5d0c0454135feaf797fcbd2b7d4f7f7efc9fdc33
Karenin1234 <chomikowskiv@gmail.com>
2025-10-25 22:04:51 +0200

    Merge remote-tracking branch 'github2/master'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 pygame.init()
 window = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Gra na historie")
 def levelOne():
     run = True
 
@@ -32,14 +33,56 @@ def levelOne():
 def main():
     run = True
     clock=0
-    background = pygame.image.load('assets/tloTest.png')
+
+    background = pygame.image.load('assets/tło.png').convert()
+    przycisk_start = pygame.image.load('assets/przycisk_start.png').convert_alpha()
+    przycisk_autorzy = pygame.image.load('assets/przycisk_autorzy.png').convert_alpha()
+
+
+    przycisk_start = pygame.transform.scale(przycisk_start, (180, 60))
+    przycisk_autorzy = pygame.transform.scale(przycisk_autorzy, (180, 60))
+
+
+    przycisk_start_rect = przycisk_start.get_rect(topleft=(320, 250))
+    przycisk_autorzy_rect = przycisk_autorzy.get_rect(topleft=(320, 350))
+
     while run:
-        clock+= pygame.time.Clock().tick(60)/1000
+        pozycja_myszki = pygame.mouse.get_pos()
+
+
+        window.blit(background, (0, 0))
+
+
+        if przycisk_start_rect.collidepoint(pozycja_myszki):
+            przycisk_start_hover = przycisk_start.copy()
+            przycisk_start_hover.fill((50, 50, 50, 50), special_flags=pygame.BLEND_RGBA_ADD)
+            window.blit(przycisk_start_hover, przycisk_start_rect)
+        else:
+            window.blit(przycisk_start, przycisk_start_rect)
+
+
+        if przycisk_autorzy_rect.collidepoint(pozycja_myszki):
+            przycisk_autorzy_hover = przycisk_autorzy.copy()
+            przycisk_autorzy_hover.fill((50, 50, 50, 50), special_flags=pygame.BLEND_RGBA_ADD)
+            window.blit(przycisk_autorzy_hover, przycisk_autorzy_rect)
+        else:
+            window.blit(przycisk_autorzy, przycisk_autorzy_rect)
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-        window.blit(background,(0,0))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if przycisk_start_rect.collidepoint(pozycja_myszki):
+                    print("Kliknięto START")
+
+
+                if przycisk_autorzy_rect.collidepoint(pozycja_myszki):
+                    print("Kliknięto AUTORZY")
+
+
+
         pygame.display.update()
 
 if __name__=="__main__":
